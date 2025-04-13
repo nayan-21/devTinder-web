41c7a08dc697e110bde8ea2ba6100aceef001380
nayan-21 <githubnayan57@gmail.com>
2025-04-13 22:18:28 +0530

    login

---
+++ src/components/Login.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser } from "../utils/userSlice";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      // console.log(res.data);
+
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="card card-dash bg-base-300 w-96 my-24 m-auto">
+      <div className="card-body">
+        <h2 className="card-title">Login</h2>
+        <label className="input validator mt-5">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+              <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+            </g>
+          </svg>
+          <input
+            type="email"
+            placeholder="mail@site.com"
+            value={emailId}
+            onChange={(e) => setEmailId(e.target.value)}
+            required
+          />
+        </label>
+        <div className="validator-hint hidden mb-3">
+          Enter valid email address
+        </div>
+        <label className="input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+              <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+            </g>
+          </svg>
+          <input
+            type="password"
+            required
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          />
+        </label>
+        <p className="validator-hint hidden">
+          Must be more than 8 characters, including
+          <br />
+          At least one number
+          <br />
+          At least one lowercase letter
+          <br />
+          At least one uppercase letter
+        </p>
+
+        <div className="card-actions justify-center my-4">
+          <button className="btn btn-primary" onClick={handleLogin}>
+            LogIn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
