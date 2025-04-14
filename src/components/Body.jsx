78339152abd1413d9e049e45778a160ddc29752a
nayan-21 <githubnayan57@gmail.com>
2025-04-14 09:24:12 +0530

    auth completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const userData = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log(res);
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
